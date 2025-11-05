48a5a26c0a3c58dd1a62f865ce791f937eebcdd3
JayCHN2004 <jehc055@aucklanduni.ac.nz>
2025-11-05 15:35:30 +1300

    added folder hover feature

--- my-app/src/components/Folder/Folder.tsx
+++ my-app/src/components/Folder/Folder.tsx
@@ -1,12 +1,19 @@
+import { useState } from "react";
+
 export function Folder(props) {
+
+  const [isHovered, setHover] = useState(false)
+
   return (
     <button
       onClick={props.onClick}
       className="flex flex-col items-center justify-center w-20 text-sm text-center hover:opacity-80"
     >
       <img
-        src="images/folder.png"
+        src = {isHovered ? "images/folder_hover.png" : "images/folder.png"}
         alt={"folder"}
+        onMouseEnter={() => setHover(true)}
+        onMouseLeave={() => setHover(false)}
       />
       <span>{props.name}</span>
     </button>
