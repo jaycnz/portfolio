14e4c7f8d7fd336780b29428e4ff06971bbb37e8
jaycnz <ijaygt@gmail.com>
2025-11-23 15:09:53 +1300

    FIX: aboutme colour override

--- src/components/WindowFactory/WindowTypes/GenAIVisuals/GenAIVisuals.tsx
+++ src/components/WindowFactory/WindowTypes/GenAIVisuals/GenAIVisuals.tsx
@@ -6,7 +6,7 @@ export function GenAIVisuals() {
   return (
     <div className="TEST flex flex-col p-4 space-y-4 overflow-scroll h-126">
         <h1 className="flex font-bold text-xl">14 Day Generative AI Visual Prompting Challenge</h1>
-        <p>blah blah blah description blah blah</p>
+        <p>I spent 14 days writing and developing prompt frameworks to effectively generate visual outputs in a product photoshoot/marketing context.</p>
         <p>blah blah blah description blah blah</p>
         <img src={'https://picsum.photos/200'}></img>
         <p>blah blah blah description blah blah</p>
