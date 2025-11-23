c1ea7e271f62add028dff6144e3cae93f65c7cbf
jaycnz <ijaygt@gmail.com>
2025-11-23 15:31:32 +1300

    WIP: carasoul posts

--- src/components/WindowFactory/WindowTypes/GenAIVisuals/GenAIVisuals.tsx
+++ src/components/WindowFactory/WindowTypes/GenAIVisuals/GenAIVisuals.tsx
@@ -6,7 +6,15 @@ export function GenAIVisuals() {
   return (
     <div className="TEST flex flex-col p-4 space-y-4 overflow-scroll h-126">
         <h1 className="flex font-bold text-xl">14 Day Generative AI Visual Prompting Challenge</h1>
-        <p>I spent 14 days writing and developing prompt frameworks to effectively generate visual outputs in a product photoshoot/marketing context.</p>
+        <p>I spent 14 days writing and developing prompt frameworks to effectively generate visual outputs in a product photoshoot/marketing context.
+          <br/>            
+          <br/>
+            jaycnz1023@gmail.com
+            <br/>
+            <a href="https://www.linkedin.com/in/jay-chen-535201270/" className="Link" target="_blank">LinkedIn</a>
+            <br/>
+            <a href="https://www.instagram.com/jayc.blend/" className="Link" target="_blank">Instagram</a>
+        </p>
         <p>blah blah blah description blah blah</p>
         <img src={'https://picsum.photos/200'}></img>
         <p>blah blah blah description blah blah</p>
