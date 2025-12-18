abd31117ff9ec87d6c090631538b8f72976d14e6
jaycnz <ijaygt@gmail.com>
2025-12-19 09:17:39 +1300

    fix: z-index reversed

--- src/components/WindowFactory/WindowFactory.tsx
+++ src/components/WindowFactory/WindowFactory.tsx
@@ -9,29 +9,30 @@ export function WindowFactory({ windows, onClose, openWindow }: { windows: any[]
   return (
     <>
       {windows.map((window, index) => {
+        const zIndex = windows.length - index;
         switch (window) {
           case 'aboutmedocument':
             return (
-              <WindowWrapper title="About me" key={window} priority={index} height={550} width={400} id={window} onClose={onClose}>
+              <WindowWrapper title="About me" key={window} priority={zIndex} height={550} width={400} id={window} onClose={onClose}>
                 <AboutMe/>
               </WindowWrapper>
             );
           case 'designs':
             return (
-              <WindowWrapper title="Designs" key={window} priority={index} height={550} width={400} id={window} onClose={onClose}>
+              <WindowWrapper title="Designs" key={window} priority={zIndex} height={550} width={400} id={window} onClose={onClose}>
                 <Designs openWindow={openWindow}/>
               </WindowWrapper>
             );
 
             case 'GenAIPrompting':
               return (
-                <WindowWrapper title="Generative AI Visual Prompting" key={window} priority={index} height={550} width={600} id={window} onClose={onClose}>
+                <WindowWrapper title="Generative AI Visual Prompting" key={window} priority={zIndex} height={550} width={600} id={window} onClose={onClose}>
                   <GenAIVisuals/>
                 </WindowWrapper>
               );
           case 'PersonalPosters':
             return (
-              <WindowWrapper title="Personal Posters" key={window} priority={index} height={650} width={600} id={window} onClose={onClose}>
+              <WindowWrapper title="Personal Posters" key={window} priority={zIndex} height={650} width={600} id={window} onClose={onClose}>
                 <PersonalPosters/>
               </WindowWrapper>
             );
